fc1d8316dcdf173c6737595551b49e6c60f0b4bf
Taha <rasheedtaha111@gmail.com>
2024-04-14 09:25:25 +0500

    School Project

---
+++ School/src/main/java/com/mycompany/school/School.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.school;
+
+/**
+ *
+ * @author Taha Rasheed
+ */
+public class School {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
